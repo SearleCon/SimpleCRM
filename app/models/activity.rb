e8a7ca9b84496af87e08c76a7baf1cc49621f3cc
Searle Consulting <shaun@searleconsulting.co.za>
2012-07-07 10:49:19 +0200

    Fixed the something went wront problem. The update for the formatting screwed up the activities and tags links. Activities controller, view and model needed fixing

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,4 @@
 class Activity < ActiveRecord::Base
   belongs_to :person
   has_and_belongs_to_many :tags
-
-  validates :shortname, :description, :targetdate, presence: true
-  validates_length_of :description, :minimum => 20, :maximum => 50
 end
-
